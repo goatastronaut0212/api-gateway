fc977baad68440f880e8f9b019e8837f32bce3d4
Đặng Tuấn Thành <tuanthanhktltk21@gmail.com>
2025-03-19 22:48:22 +0700

    init api company service

--- src/bus-management-service/company.controller.ts
+++ src/bus-management-service/company.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('v2/company')
@@ -10,5 +19,25 @@ export class CompanyController {
   createCompany(@Body() data: any) {
     console.log('Sending request to Bus Management Service:', data);
     return this.company.send('create_company', data);
+  }
+  @Get('/get-all')
+  getAllCompanies() {
+    return this.company.send('get_all_companies', {});
+  }
+  @Put('/update-company/:id')
+  updateCompany(@Param('id') id: number, @Body() data: any) {
+    return this.company.send('update_company', { id, ...data });
+  }
+  @Delete('/delete-company/:id')
+  deleteCompany(@Param('id') id: number) {
+    return this.company.send('delete_company', { id });
+  }
+  @Put('/lock-company/:id')
+  lockCompany(@Param('id') id: number) {
+    return this.company.send('lock_company', { id });
+  }
+  @Put('/unlock-company/:id')
+  unlockCompany(@Param('id') id: number) {
+    return this.company.send('unlock_company', { id });
   }
 }
