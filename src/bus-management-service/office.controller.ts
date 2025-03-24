07419fcbb015d62f7a70609d7861ffff8e56807a
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2025-03-24 11:04:05 +0700

    init office controller

--- src/bus-management-service/office.controller.ts
+++ src/bus-management-service/office.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('v2/office')
@@ -10,5 +19,22 @@ export class OfficeController {
   createOffice(@Body() data: any) {
     console.log('Sending request to Bus Management Service:', data);
     return this.office.send('create_office', data);
+  }
+  @Get('/get-office-by-company/:companyId')
+  getOfficesByCompany(@Param('companyId') companyId: any) {
+    console.log('Sending request to Bus Management Service:', companyId);
+    return this.office.send('get_office_by_company', { companyId });
+  }
+  @Get('/get-office-name-by-company/:companyId')
+  getOfficeNameByCompany(@Param('companyId') companyId: number) {
+    return this.office.send('get_office_name_by_company', { companyId });
+  }
+  @Put('/update-office/:id')
+  updateOffice(@Param('id') id: number, @Body() data: any) {
+    return this.office.send('update_office', { id, ...data });
+  }
+  @Delete('/delete-office/:id')
+  deleteOffice(@Param('id') id: number) {
+    return this.office.send('delete_office', { id });
   }
 }
